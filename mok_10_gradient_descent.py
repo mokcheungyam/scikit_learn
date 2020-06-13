a09c622dd34fa3e90eb2a2ad2357704c43efaac9
mokcheungyam <609356067@qq.com>
2020-06-13 16:33:10 +0800

    正规方程线性回归预测Boston房价

---
+++ mok_10_gradient_descent.py
@@ -0,0 +1,17 @@
+#! /usr/local/bin/python3
+# -*- coding:utf-8 -*-
+
+"""
+File Name: mok_10_tiduxiajiang.py 
+Author: mok
+Creation Date: 2020/6/13
+Description: 
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
